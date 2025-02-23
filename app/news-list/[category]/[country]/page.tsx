42eca5cb110894b211f83f5759ecca5dc00569e1
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-02-23 13:20:01 +0200

    loading (convention files)

--- app/news-list/[category]/[country]/page.tsx
+++ app/news-list/[category]/[country]/page.tsx
@@ -1,27 +1,14 @@
+import fetchNews from '@/app/services/news.service';
 import Fetching from '@/components/Fetching/NewsList';
 interface IProps {
     params: Promise<{category: string, country: string}>
 }
 const NewsList = async (props: IProps) => {
-    const params = await props.params;
-    const category = params.category;
-    const country = params.country;
-    let loading = true;
-    const getNews = async () => {
-        const res = await fetch(`https://newsdata.io/api/1/latest?apikey=pub_703950700639eec12e7bff9628002924463bd&category=${category}&country=${country}`);
-        const data: News.IResponse = await res.json();
-        loading = false;
-        return data.results.map(item => ({
-            id: item.article_id,
-            title: item.title,
-            img: item.image_url,
-            content: item.description
-        })) as News.Item[];
-    }
-    const news = await getNews();
-    if(loading) return <div>Loading....</div>;
+    const {category, country} = await props.params;
+    const latestNews = await fetchNews(category, country);
+
     return (
-        <Fetching news={news} country={country} category={category}/>
+        <Fetching news={latestNews} country={country} category={category}/>
     )
 }
 

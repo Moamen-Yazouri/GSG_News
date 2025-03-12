6356cbbf7dce24c7c0304c0e418abcc3346ad623
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-03-12 12:05:56 +0200

    Getting news by category using query params.

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,11 +1,9 @@
 import { ALLOWEDCATEGORIES } from "@/app/constants/data";
 import { getNews } from "@/app/services/news.services";
 import { NextRequest, NextResponse } from "next/server"
-interface IProps {
-    params: Promise<{category:string}>;
-}
-export const GET = async (request: NextRequest, props: IProps) => {
-    const category = (await props.params).category
+export const GET = async (request: NextRequest) => {
+    const params = request.nextUrl.searchParams;
+    const category = params.get("category") || "global";
     if(!ALLOWEDCATEGORIES.includes(category)) {
         return NextResponse.json(null, {status:400, statusText: "Invalid Category"})
     }
